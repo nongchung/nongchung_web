405e5d439100b701a2e40e376c84eb2ef8e64442
배지원 <bjiwon2005@naver.com>
2018-07-01 15:43:25 +0900

    회원가입 작업 1차

--- src/components/user/auth/Register.vue
+++ src/components/user/auth/Register.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-        회원가입페이지입니다.
+        <h1>회원가입하기</h1>
+    <input type="email" v-model="email" placeholder="email"><br>
+    <input type="text" v-model="password" placeholder="password"><br>
+    <input type="text" v-model="nickname" placeholder="nickname"><button @click="dup">중복확인</button><br>
+    <input type="text" v-model="name" placeholder="name"><br>
+    <input type="text" v-model="sex" placeholder="sex"><br>
+    <input type="text" v-model="hp" placeholder="hp"><br>
+    <input type="text" v-model="birth" placeholder="birth"><br>
+    <button @click="localRegister">가입하기</button>
     </div>
 </template>
 
 <script>
 export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      nickname: '',
+      password: '',
+      name: '',
+      sex: '',
+      hp: '',
+      birth: ''
+    }
+  },
+  computed: {
 
+  },
+  methods: {
+    dup () {
+      const email = this.email
+      this.$store.dispach('dup', {email})
+    }
+    // localRegister () {
+    //   const { email, password, displayName } = this
+    //     this.$store.dispatch('register', { email, password, displayName })
+    //   },
+    // dup () {
+
+    // }
+  },
+  mounted () {
+
+  }
 }
 </script>
 
