c8da87287a1eb4b9c96b360ab54d6caf3c13e03b
배지원 <bjiwon2005@naver.com>
2018-07-04 21:27:13 +0900

    디렉터리 정리

---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<!-- <v-container fluid> -->
+    <v-layout column>
+        <!-- <search-bar></search-bar> -->
+        <main-ad></main-ad>
+        <popular-list></popular-list>
+        <new-list></new-list>
+        <popular-farm></popular-farm>
+    </v-layout>
+    <!-- </v-container> -->
+</template>
+
+<script>
+import SearchBar from './search/SearchBar'
+import MainAd from './advertisement/MainAd'
+import PopularList from './list/PopularList'
+import NewList from './list/NewList'
+import PopularFarm from './list/PopularFarm'
+export default {
+  components: {
+    SearchBar,
+    MainAd,
+    PopularList,
+    NewList,
+    PopularFarm
+  },
+  created () {
+    this.$store.dispatch('getMain')
+  }
+}
+</script>
+
+<style>
+</style>
