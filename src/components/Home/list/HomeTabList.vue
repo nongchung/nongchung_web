647425b6e0f85bf9b96c6532e5d0ebc0659d276d
crystal_rim <remember2889@gmail.com>
2018-07-05 16:25:41 +0900

    난뭘하고있능가

--- src/components/Home/list/HomeTabList.vue
+++ src/components/Home/list/HomeTabList.vue
@@ -1,9 +1,9 @@
 <template>
  <div>
     <v-tabs slot="extension" v-model="tabnumber" color="white" slider-color="cyan" id="home_tab">
-      <span style="margin: 5vw;"></span>
+      <v-flex sm1 md1 lg2 xl2></v-flex>
             <v-tab :ripple="false" v-for="i in 3" :key="i" :href="`#tab-${i}`">
-        {{tabname[i-1]}}
+        <span style="font-size:1.2rem; font-family:san-serif; font-weight:700;">{{tabname[i-1]}}</span>
       </v-tab>
     </v-tabs>
     <v-tabs-items v-model="tabnumber">
