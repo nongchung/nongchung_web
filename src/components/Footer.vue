d9c3fe128eae511a3e7da89ec94ea3ae3cbb4dc4
crystal_rim <remember2889@gmail.com>
2018-07-05 14:12:00 +0900

    07051411

---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-footer
+    dark
+    height="auto"
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <v-layout ma-4 px-5>
+        <strong class="subheading ma-2 px-5">농촌에서 활짝피는 청춘, 농활청춘<br>&copy;2018 — <strong>nonghwal</strong></strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-2"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+</v-layout>
+      </v-card-title>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      icons: [
+        'fab fa-facebook',
+        'fab fa-instagram'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-toolbar app flat fixed color="white" height="80px;">
+      <router-link to="/" tag="a" class="hidden-sm-and-up">
+                <img src="../assets/logo_ex.png" height="30px" style="margin-top: 10px;">
+      </router-link>
+      <v-toolbar-title id="toolbar_title" style="margin-right: 1vw;" class="hidden-xs-only">
+        <router-link to="/" tag="a" >
+                <img src="../assets/logo_ex.png" height="35px" style="margin-top: 10px;">
+      농활청춘
+      </router-link>
+      </v-toolbar-title>
+          <v-btn top :ripple="false" flat class="toolbar_list" exact router to="#1" color="grey lighten-1" active-class="black" style="padding-left: 20px !important;">
+            농활청춘소개
+          </v-btn>
+          <v-btn top :ripple="false" flat class="toolbar_list" exact router to="#2" color="grey lighten-1" active-class="black">
+            포인트샵
+          </v-btn>
+          <v-btn top :ripple="false" flat class="toolbar_list" exact router to="#3" color="grey lighten-1" active-class="black">
+            이용안내
+          </v-btn>
+      <v-spacer></v-spacer>
+      <v-flex sm3 md2 lg2>
+      <v-text-field
+        append-icon="search"
+        class="search_bar mx-3"
+        flat
+        placeholder="농활 검색"
+        solo
+        color="black"
+        v-if="searchbar"
+      ></v-text-field></v-flex>
+          <v-btn top :ripple="false" flat class="toolbar_list" color="grey lighten-1" active-class="black"
+          v-if="searchicon" @click="showSearch()">
+            <v-icon>search</v-icon>
+          </v-btn>
+          <v-btn top :ripple="false" flat class="toolbar_list" exact router to="/login" color="grey lighten-1" active-class="black"
+          v-if="!isAuthenticated">
+            로그인
+          </v-btn>
+          <v-btn top :ripple="false" flat class="toolbar_list" exact router to="/register" color="grey lighten-1" active-class="black"
+          v-if="!isAuthenticated">
+            회원가입
+          </v-btn>
+          <v-btn top :ripple="false" flat class="toolbar_list" exact router to="/mypage" color="grey lighten-1" active-class="black"
+          v-if="isAuthenticated">
+            마이페이지
+          </v-btn>
+          <v-btn top :ripple="false" flat class="toolbar_list" exact @click="logout" color="grey lighten-1" active-class="black"
+          v-if="isAuthenticated">
+            로그아웃
+          </v-btn>
+
+    </v-toolbar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  data () {
+    return {
+      searchicon: true,
+      searchbar: false
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'isAuthenticated'
+    ])
+  },
+  methods: {
+    logout () {
+      const isOut = confirm('로그아웃 하시겠습니까?')
+      if (isOut) {
+        this.$store.dispatch('logout')
+      }
+    },
+    showSearch () {
+      this.searchicon = false
+      this.searchbar = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+ #toolbar_title{
+      font-weight: 900;
+      font-size: 42px;
+      margin-top: 9px;
+      overflow: visible;
+    }
+    .toolbar_list{
+      font-size: 1rem;
+    }
+    .v-btn--active:before,.v-btn:focus:before,.v-btn:hover:before{
+      background-color:transparent !important;
+      }
+    .v-btn:before,.v-btn:focus:before,.v-btn:hover:before{
+      background-color:transparent !important;
+      }
+    .black{
+      color: black !important;
+    }
+    .toolbar_text{
+      margin-top: 1rem;
+      font-weight:800;
+    }
+    /* .application.theme--light{
+      background: #fff !important;
+    } */
+    /* .v-input-slot:before{
+      color: transparent !important;
+    }
+    .v-text-field__slot{
+      border-bottom-color: white !important;
+    }
+    .v-input__is-focus{
+      caret-color: black;
+    } */
+    .search_bar, .search_bar:active{
+      font-size: .9rem !important;
+      border-bottom: 1px solid black !important;
+      height: 40px !important;
+      margin-top: 23px !important;
+    }
+    a{
+      text-decoration: none;
+      color:darkblue;
+    }
+</style>
