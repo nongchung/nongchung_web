4bbebfa293c15904d8f3a70c32ba8e8c5623bbf2
배지원 <bjiwon2005@naver.com>
2018-07-08 20:40:21 +0900

    후기작성 - 3

--- src/components/Review/WritereviewForm.vue
+++ src/components/Review/WritereviewForm.vue
@@ -1,22 +1,21 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs5 offset-xs3>
+  <v-layout row justify-center>
+    <v-flex xs6 mt-4>
       <v-card>
       <v-card-title class="headline font-weight-regular white--text title"><span class="titleFont">농활 정보</span></v-card-title>
+      <v-layout row justify-center pa-4>
       <v-flex>
-        <v-avatar
-          :tile="tile"
-          :size="avatarSize"
-          color="grey lighten-4"
-        >
-          <img src="/apple-touch-icon-180x180.png" alt="avatar">
-        </v-avatar>
+        <v-card-media :src="nhDeatail.image[0]" height="151px" width="253px"></v-card-media>
       </v-flex>
       <v-flex>
       <v-card-text>
+        <v-layout column>
         <v-subheader class="pa-0">Where do you live?</v-subheader>
+        <v-subheader class="pa-0">Where do you live?</v-subheader>
+        </v-layout>
       </v-card-text>
       </v-flex>
+      </v-layout>
     </v-card>
       <v-card>
       <v-card-title class="headline font-weight-regular white--text title"><span class="titleFont">농활 후기</span></v-card-title>
@@ -29,15 +28,13 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'Writereview',
   computed: {
-    getPath: function () {
-      return this.$route.params.idx
-    }
-  },
-  created () {
-    this.$store.dispatch('nonghwalDetail')
+    ...mapGetters({
+      nhDeatail: 'getNonghwalDetail'
+    })
   }
 }
 </script>
