005d99a584097eb7cc5740f80ea5e309953a7661
배지원 <bjiwon2005@naver.com>
2018-07-14 13:40:11 +0900

    layout

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout row justify-center>
+  <v-layout row justify-center style="background-color:white; padding-bottom:80px">
     <v-flex xs12 sm12 md10 lg8 xl8>
       <img class="hidden-xs-only mt-5" src="../../static/nh_info.jpeg" width="100%">
       <img class="hidden-sm-and-up mt-4" src="../../static/nh_info_m.png" width="100%">
